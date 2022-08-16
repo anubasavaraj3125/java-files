bdf4f0ff1a8973a526fea602f14564252c36f34d
anubasavaraj3125 <110526861+anubasavaraj3125@users.noreply.github.com>
2022-08-16 15:02:21 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,30 @@
+public interface A {
+	void a();
+	void b();
+	void c();
+	void d();
+
+}
+abstract class B implements A 
+{
+	public void c() {System.out.println("I Am c");}
+}
+class C extends B {
+
+	public void a() {System.out.println("I am A");}
+	public void b() {System.out.println("I am B");}
+	public void d() {System.out.println("I am D");}
+}
+
+
+class Testabstarct{
+public static void main(String[] args) {
+	A a=new C();
+	a.a();
+	a.b();
+	a.c();
+	a.d();
+	
+}
+
+}
